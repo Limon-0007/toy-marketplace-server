e84d925416b9cb6cb99d41f5e8cc2ee8819d7403
Limon-0007 <limonahmed3023@gmail.com>
2023-05-19 15:18:37 +0600

    midified some operations

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ async function run() {
     app.delete("/addAToy/:id", async (req, res) => {
       const id = req.params.id
       const query = {_id: new ObjectId(id)}
-      const result = toyCollection.deleteOne(query)
+      const result = await toyCollection.deleteOne(query)
       res.send(result)
     })
 
